b1f2e50d2da324c7f3b6548528eda4b702ca48b7
Łukasz Wiktor <lukasz.wiktor@gmail.com>
2016-12-10 10:06:16 +0100

    CodeRain implemenation and docs

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # coderain
 a javascript library to genarate unique random codes
+
+
+### Usage
+
+First you need to create a new instance of `CodeRain` specifying a code pattern.
+In the example we are going to generate codes consisting of 3 random uppercase
+letters followed by a hyphen and 5 random digits.
+
+```
+var cr = new CodeRain("AAA-99999");
+```
+
+Then you can keep calling `next()` and it's guaranteed that generated codes are
+unique, i.e. they don't repeat for given `CodeRain` instance.
+
+For example to get an array of 1000 unique random codes:
+
+```
+var codes = [];
+while (codes.length < 1000) {
+    codes.push(cr.next());
+}
+```
+
+### Patterns
+
+| placeholder | possible characters                    | description                |
+|-------------|----------------------------------------|----------------------------|
+| `A`         | `ABCDEFGHIJKLMNOPQRSTUVWXYZ`           | uppercase letters          |
+| `a`         | `abcdefghijklmnopqrstuvwxyz`           | lowercase letters          |
+| `9`         | `0123456789`                           | digits                     |
+| `X`         | `ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789` | uppercase letters + digits |
+| `x`         | `abcdefghijklmnopqrstuvwxyz0123456789` | lowercase letters + digits |
+
+If you would like to use reserved characters in you pattern you can quote it with
+single quotes, for example:
+
+`var xmasCodes = new CodeRain("'XMAS'-XXXXX")`
+
+
+### License
+
+Code released under the [MIT license](LICENSE).
---
+++ coderain.js
@@ -0,0 +1,67 @@
+;(function() {
+    var placeholders = {
+        "A": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        "a": "abcdefghijklmnopqrstuvwxyz",
+        "9": "0123456789",
+        "X": "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        "x": "abcdefghijklmnopqrstuvwxyz0123456789"
+    }
+
+    function randomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+    
+    function randomElem(array) {
+        return array[randomInt(array.length)];
+    }
+
+    function randomCode(pattern) {
+        var code = "";
+        var quote = "'";
+        var quoteOpen = false;
+        pattern.split("").forEach(function(c) {
+            if (!quoteOpen) {
+                if (placeholders[c]) {
+                    code += randomElem(placeholders[c]);
+                } else if (c === quote) {
+                    quoteOpen = true;
+                } else {
+                    code += c;
+                }
+            } else {
+                if (c === quote) {
+                    quoteOpen = false;
+                } else {
+                    code += c;
+                }
+            }
+        });
+        return code;
+    }
+    
+    function CodeRain(pattern) {
+        var cache = {};
+        var maxAttempts = 0xBAD;
+        this.next = function() {
+            var attempt = 0;
+            
+            do {
+                var code = randomCode(pattern);
+            } while (cache[code] && ++attempt < maxAttempts);
+            
+            cache[code] = true;
+            
+            if (attempt === maxAttempts) {
+                throw new Error("CodeRain: cannot generate next unique code.")
+            }
+            
+            return code;
+        }
+        this.reset = function() {
+            cache = {};
+        }
+    }
+    
+    this.CodeRain = CodeRain;
+    
+}).call(this);
